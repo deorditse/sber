d66cb2e4dd20f61788262bdf1c15b917be4574fb
deorditse.d <deorditse.d@yandex.com>
2022-12-08 15:00:13 +0300

    конфигурация для удаленного сервера

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:get/get.dart';
 import 'package:sber/ui_layout/pages/subscription_check_page/subscription_check_page.dart';
 import 'package:business_layout/business_layout.dart';
 import 'ui_layout/style_app/theme_app/material_theme/light_custom_theme_material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 //flutter build web
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized(); //for future
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //for future
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyGetAppSubscription());
 }
 
